52103a4a969afac9ab3db4afba2ead04d039854c
27 <zhengchengzhong@yuewen.com>
2019-09-28 21:30:02 +0800

    first: commit

---
+++ code.ts
@@ -0,0 +1,199 @@
+const _api = {
+
+  // This is a generator that recursively produces
+  // all the nodes in subtree starting at the given node
+  walkTree: function* (node: { children: any; }) {
+    yield node;
+    let children = node.children;
+    if (children) {
+      for (let child of children) {
+        yield* this.walkTree(child);
+      }
+    }
+  },
+  getNodesByType: function ({ type, nodes }) {
+    let results = [];
+    nodes.map((node) => {
+      let walker = this.walkTree(node);
+      let res: { value: any; };
+      while (!(res = walker.next()).done) {
+        const matchNode = res.value;
+        if (matchNode.type === 'TEXT') {
+          results.push(matchNode);
+        }
+      }
+      return node;
+    });
+    return results;
+  },
+  getTextAlignClassByProp: function (textAlignHorizontal: string) {
+    const stringMap = {
+      "LEFT": false,
+      "CENTER": 'tac',
+      "RIGHT": 'tar',
+      "JUSTIFIED": 'taj'
+    };
+    return stringMap[textAlignHorizontal];
+  },
+  getTextDecorationClassByProp: function (textDecoration: string) {
+    const stringMap = {
+      "NONE": false,
+      "UNDERLINE": 'tdu',
+      "STRIKETHROUGH": 'td'
+    };
+    return stringMap[textDecoration];
+  },
+  getTextCaseClassByProp: function (textCase: string) {
+    const stringMap = {
+      "ORIGINAL": false,
+      "LOWER": 'tac',
+      "UPPER": 'tar',
+      "TITLE": 'taj'
+    };
+    return stringMap[textCase];
+  },
+  getTextFontStyleArrayClassByProp: function ({ style }) {
+    if (!style) {
+      return [];
+    }
+    const classArray = [];
+    const fontStyleMap = {
+      "UltraLight": 'fw100',
+      "Thin": 'fw200',
+      "Light": 'fw300',
+      "Regular": false,
+      "Medium": 'fw500',
+      "SemiBold": 'fw600',
+      "Bold": 'fw700',
+      "Black": 'fw900',
+      "Italic": 'fsi',
+      "Oblique": 'fsi'
+    };
+    const fontStyleArray = style.split(' ');
+    fontStyleArray.map((item: string) => {
+      const fontStyleClass = fontStyleMap[item];
+      fontStyleClass && classArray.push(fontStyleClass);
+      return item;
+    });
+    return classArray;
+  },
+  getPropByNode: function (node: any, name: string) {
+    const nameMap = {
+      'textCase': 'getRangeTextCase',
+      'lineHeight': 'getRangeLineHeight',
+      'letterSpacing': 'getRangeLetterSpacing',
+      'fontSize': 'getRangeFontSize',
+      'textDecoration': 'getRangeTextDecoration',
+      'fontName': 'getRangeFontName',
+      'fills': 'getRangeFills'
+    }
+
+    if (typeof node[name] === 'symbol' && nameMap[name] && typeof node[nameMap[name]] === 'function') {
+      return node[nameMap[name]](0, 1);
+    }
+    return node[name];
+  },
+  getNodeClassArray: function (node: any) {
+    let classArray = [];
+
+    // textAlign
+    const textAlignClass = this.getTextAlignClassByProp(node.textAlignHorizontal);
+    textAlignClass && classArray.push(textAlignClass);
+
+    // textCase
+    const textCase = this.getPropByNode(node, 'textCase');
+    const textCaseClass = this.getTextCaseClassByProp(textCase);
+    textCaseClass && classArray.push(textCaseClass);
+
+    // textDecoration
+    const textDecoration = this.getPropByNode(node, 'textDecoration');
+    const textDecorationClass = this.getTextDecorationClassByProp(textDecoration);
+    textDecorationClass && classArray.push(textDecorationClass);
+
+    // color
+    const paintStyle = figma.getLocalPaintStyles().find((item) => item.id === node.fillStyleId);
+    paintStyle && classArray.push('c_' + paintStyle.name.toLowerCase());
+
+    // fontStyle 
+    const fontName = this.getPropByNode(node, 'fontName');
+    const fontStyleClassArray = this.getTextFontStyleArrayClassByProp(fontName);
+    classArray = classArray.concat(fontStyleClassArray);
+
+
+    // fontSize 
+    const fontSize = this.getPropByNode(node, 'fontSize');
+    if (typeof fontSize === 'number' && fontSize > 11) {
+      classArray.push('fs' + fontSize);
+    }
+
+    // lineHeight 
+    const lineHeight = this.getPropByNode(node, 'lineHeight');
+    if (lineHeight.unit === "PIXELS" && lineHeight.value > fontSize) {
+      classArray.push('lh' + lineHeight.value);
+    } else if (lineHeight.unit === "PERCENT" && lineHeight.value > 100) {
+      classArray.push('lh' + lineHeight.value + 'p');
+    }
+
+    return classArray;
+  },
+  getClassPropsString: function (classArray: any[]) {
+    if (!classArray.length) {
+      return '';
+    }
+    if (classArray.length === 1) {
+      return ` class="${classArray[0]}"`;
+    }
+    return ` class="${classArray.join(' ')}"`;
+  },
+  getHtmlByTextNode: function (node: { characters: string, paragraphSpacing: number }) {
+    console.log(node);
+    let containerClassArray = this.getNodeClassArray(node);
+    const containerClassString = this.getClassPropsString(containerClassArray);
+    const pragraphs = node.characters.split('\n');
+
+    // 只有一段
+    if (pragraphs.length === 1) {
+      return `<p${containerClassString}>${pragraphs[0]}</p>`;
+    }
+
+    // 段间距
+    let pMarginClassString = '';
+    const paragraphSpacing = this.getPropByNode(node, 'paragraphSpacing');
+    if (paragraphSpacing > 0) {
+      pMarginClassString = ` class="mb${paragraphSpacing}"`;
+    }
+
+    // 多段
+    let html = '';
+    pragraphs.map((pragraph) => {
+      html += `    <p${pMarginClassString}>${pragraph}</p>\n`;
+    });
+
+    return `<div${containerClassString}>\n${html}</div>`;
+  },
+  getHtmlByTextNodes: function (nodes: any[]) {
+    let html = "";
+    nodes.map((node: SceneNode) => {
+      html += this.getHtmlByTextNode(node);
+      return node;
+    });
+    return html;
+  },
+  start: function () {
+    const selections = figma.currentPage.selection;
+    const selectLength = selections.length;
+    if (!selectLength) {
+      return '😢 Please select something';
+    }
+    const textNodes = this.getNodesByType({
+      type: 'TEXT',
+      nodes: selections
+    });
+    const html = this.getHtmlByTextNodes(textNodes);
+    console.log(html);
+    return '';
+  }
+}
+
+const msg = _api.start();
+// figma.closePlugin(msg);
