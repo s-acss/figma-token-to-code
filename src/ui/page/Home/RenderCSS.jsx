3fb47d020ccc43e9b31d8a31e2b0dda1fed386e7
27 <zhengchengzhong@yuewen.com>
2021-02-24 12:51:01 +0800

    feat: 修改ACSS 到 sacss

--- src/ui/page/Home/RenderCSS.jsx
+++ src/ui/page/Home/RenderCSS.jsx
@@ -8,6 +8,15 @@ import NoSelection from "./NoSelection";
 
 
 const DEFAULT_COPY = 'Copy';
+
+const CODE_PRE=`/*
+ * All rules in 「 sacss 」 will filter out.
+ * Make sure you got 「 scass 」 in your project
+ * $ npm install sacss
+ * @import '~sacss';
+ */\n
+`;
+
 const RenderCSS = ({noSelection = true, code}) => {
 
   const [copyText, setCopyText] = useState(DEFAULT_COPY);
@@ -25,12 +34,14 @@ const RenderCSS = ({noSelection = true, code}) => {
       {noSelection ? <NoSelection/> : (
         <div className="f1 oa" style={{backgroundColor: '#282c34'}}>
           <Highlight languages={["css"]} className="css">
-            {`/* @import '~sacss'; */\n\n${code||''}`}
+            {`${CODE_PRE}${code||''}`}
           </Highlight>
         </div>
       )}
       <div className="g_row df aic pt12 pb12 bc:fff">
-        <a className="btn mr8" href="https://www.npmjs.com/package/sacss" target="_blank">NPM SACSS</a>
+        <a className="mr8" href="https://www.npmjs.com/package/sacss" target="_blank">
+          <img src="https://img.shields.io/npm/v/sacss.svg" width="80" height="20" alt="sacss"/>
+        </a>
         <Button
           className="f1"
           disabled={noSelection || copyText !== DEFAULT_COPY}
